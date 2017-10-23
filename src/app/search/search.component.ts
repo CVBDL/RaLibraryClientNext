c079e9ce0af30053dcd63e90cf55ac224be1d64e
zhongsp <patrick.zhongsp@gmail.com>
2017-10-23 13:23:23 +0800

    prototype

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,16 +1,38 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Book } from "../shared/book";
+import { BooksService } from "../core/books.service";
 
 @Component({
   selector: 'ral-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  books: Book[];
+  keyword: string = '';
 
-  constructor() { }
+  private isSearching: boolean = false;
 
-  ngOnInit() {
+  constructor(private booksService: BooksService) { }
+
+  search(keyword: string): void {
+    this.isSearching = true;
+    this.booksService.list().subscribe(data => {
+      this.books = this.filter(data, keyword);
+      this.isSearching = false;
+    }, () => this.isSearching = false);
+  }
+
+  private filter(books: Book[], keyword: string): Book[] {
+    var filteredBooks: Book[] = [];
+    books.forEach(book => {
+      console.log(book.Title.toLowerCase(), keyword)
+      if (book.Title.toLowerCase().includes(keyword)) {
+        filteredBooks.push(book);
+      }
+    });
+    return filteredBooks;
   }
 
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { SearchRoutingModule } from "./search-routing.module";
 import { SharedModule } from "../shared/shared.module";
@@ -9,6 +10,7 @@ import { SearchComponent } from './search.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     SearchRoutingModule,
     SharedModule
   ],
